9225e0ff78ffda2a4e642c992f33dd3135a0ce34
Akira Matsuda <ronnie@dio.jp>
2008-11-27 00:30:42 +0900

    Updated translation logic.

--- generators/i18n_models/lib/translator.rb
+++ generators/i18n_models/lib/translator.rb
@@ -12,7 +12,7 @@ module I18nModelsGeneratorModule
         w = CGI.escape ActiveSupport::Inflector.humanize(word)
         json = OpenURI.open_uri("http://ajax.googleapis.com/ajax/services/language/translate?v=1.0&q=#{w}&langpair=en%7C#{@lang}").read
         result = ActiveSupport::JSON.decode(json)
-        return @cache[word] = result['responseData']['translatedText'] if result['responseStatus'] == 200
+        result['responseStatus'] == 200 ? (@cache[word] = result['responseData']['translatedText']) : word
       rescue => e
         puts %Q[failed to translate "#{word}" into "#{@lang}" language.]
         word
---
+++ spec/translator_spec.rb
@@ -0,0 +1,44 @@
+$KCODE = 'U'
+
+require File.join(File.dirname(__FILE__), '../generators/i18n_models/lib/translator')
+include I18nModelsGeneratorModule
+
+describe Translator do
+  before(:each) do
+    @translator = Translator.new 'ja'
+  end
+
+  describe 'when successfully translated' do
+    before do
+      res_200 = mock('res_200')
+      res_200.stub!(:read).and_return('{"responseData": {"translatedText":"こんにちは"}, "responseDetails": null, "responseStatus": 200}')
+    end
+
+    it 'returns translated text' do
+      @translator.translate('hello').should == 'こんにちは'
+    end
+  end
+
+  describe 'when translation failed with error code' do
+    before do
+      res_500 = mock('res_500')
+      res_500.stub!(:read).and_return('{"responseData": {"translatedText":"こんにちは？"}, "responseDetails": null, "responseStatus": 500}')
+      OpenURI.stub!(:open_uri).and_return(res_500)
+    end
+
+    it 'returns the original text' do
+      @translator.translate('hello').should == 'hello'
+    end
+  end
+
+  describe 'when translation raised an error' do
+    before do
+      OpenURI.stub!(:open_uri).and_raise('ERROR!')
+    end
+
+    it 'returns the original text' do
+      @translator.translate('hello').should == 'hello'
+    end
+  end
+end
+

72c3a44a22545ab8286719d6d6deca7b3ddd3e76
YAMAGUCHI, Seiji <valda68k@gmail.com>
2008-12-12 12:15:07 +0900

    trim_mode = '-'

--- generators/i18n_translation/lib/erb_executer.rb
+++ generators/i18n_translation/lib/erb_executer.rb
@@ -13,7 +13,7 @@ module I18nTranslationGeneratorModule
             nil.class_eval do
               def method_missing(method, *args, &block); nil; end
             end
-            def_erb_method 'execute', '#{filename}'
+            def_erb_method 'execute', ERB.new('#{filename}', nil, '-')
           end
         EOS
         m.const_get('Executer').new.execute { }

c85c5c287da8cebcf057ff7d490fb5d0301eb30a
Akira Matsuda <ronnie@dio.jp>
2009-02-14 02:40:40 +0900

    Ensure to remove nil.method_missing

--- generators/i18n_translation/lib/erb_executer.rb
+++ generators/i18n_translation/lib/erb_executer.rb
@@ -11,23 +11,19 @@ module I18nTranslationGeneratorModule
             include ActionView::Helpers::TranslationHelper
             include I18nTranslationGeneratorModule::ThroughRyoku
 
-            nil.class_eval do
-              def method_missing(method, *args, &block); nil; end
-            end
-
             fname = '#{filename}'
             erb = nil
             File.open(fname) {|f| erb = ERB.new(f.read, nil, '-') }
             erb.def_method(self, 'execute', fname)
           end
         EOS
+        nil.class_eval {def method_missing(method, *args, &block); nil; end}
         m.const_get('Executer').new.execute { }
-        nil.class_eval do
-          undef :method_missing
-        end
       rescue => e
         p e
         # do nothing
+      ensure
+        nil.class_eval {undef :method_missing} if nil.respond_to? :method_missing
       end
     end
   end

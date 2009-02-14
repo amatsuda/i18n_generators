05c15907cde2b16e5091bc0ba9201cd91d059bd5
Akira Matsuda <ronnie@dio.jp>
2009-02-15 04:39:33 +0900

    Use built-in JSON parser when Ruby 1.9 in order to avoid Rails bandled broken JSON parser

--- generators/i18n_translation/lib/translator.rb
+++ generators/i18n_translation/lib/translator.rb
@@ -1,4 +1,5 @@
 require 'open-uri'
+require 'json' if RUBY_VERSION >= '1.9'
 
 module I18nTranslationGeneratorModule
   class Translator
@@ -11,7 +12,11 @@ module I18nTranslationGeneratorModule
       begin
         w = CGI.escape ActiveSupport::Inflector.humanize(word)
         json = OpenURI.open_uri("http://ajax.googleapis.com/ajax/services/language/translate?v=1.0&q=#{w}&langpair=en%7C#{@lang}").read
-        result = ActiveSupport::JSON.decode(json)
+        result = if RUBY_VERSION >= '1.9'
+          ::JSON.parse json
+        else
+          ActiveSupport::JSON.decode(json)
+        end
         result['responseStatus'] == 200 ? (@cache[word] = result['responseData']['translatedText']) : word
       rescue => e
         puts %Q[failed to translate "#{word}" into "#{@lang}" language.]

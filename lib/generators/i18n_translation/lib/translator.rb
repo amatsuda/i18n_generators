20645aa513bd984566b6653e0d54d70912a661ae
Akira Matsuda <ronnie@dio.jp>
2011-11-11 10:54:50 +0900

    modulize GoogleTranslate
    to make it possible to switch to other translation engines

--- lib/generators/i18n_translation/lib/translator.rb
+++ lib/generators/i18n_translation/lib/translator.rb
@@ -1,27 +1,47 @@
 require 'open-uri'
-require 'json' if RUBY_VERSION >= '1.9'
 
 module I27r
+  class TranslationError < StandardError; end
+
+  module GoogleTranslate
+    def _translate(word, lang)
+      w = CGI.escape ActiveSupport::Inflector.humanize(word)
+      json = OpenURI.open_uri("http://ajax.googleapis.com/ajax/services/language/translate?v=1.0&q=#{w}&langpair=en%7C#{lang}").read
+      result = if RUBY_VERSION >= '1.9'
+        require 'json'
+        ::JSON.parse json
+      else
+        ActiveSupport::JSON.decode(json)
+      end
+      if result['responseStatus'] == 200
+        result['responseData']['translatedText']
+      else
+        raise TranslationError.new result.inspect
+      end
+    end
+  end
+
   class Translator
+    include GoogleTranslate
+
     def initialize(lang)
       @lang, @cache = lang, {}
     end
 
     def translate(word)
       return @cache[word] if @cache[word]
-      begin
-        w = CGI.escape ActiveSupport::Inflector.humanize(word)
-        json = OpenURI.open_uri("http://ajax.googleapis.com/ajax/services/language/translate?v=1.0&q=#{w}&langpair=en%7C#{@lang}").read
-        result = if RUBY_VERSION >= '1.9'
-          ::JSON.parse json
-        else
-          ActiveSupport::JSON.decode(json)
-        end
-        result['responseStatus'] == 200 ? (@cache[word] = result['responseData']['translatedText']) : word
-      rescue => e
-        puts %Q[failed to translate "#{word}" into "#{@lang}" language.]
+
+      translated = _translate word, @lang
+      if translated.blank? || (translated == word)
         word
+      else
+        @cache[word] = translated
+        translated
       end
+    rescue => e
+      Rails.logger.debug e
+      puts %Q[failed to translate "#{word}" into "#{@lang}" language.]
+      word
     end
   end
 end

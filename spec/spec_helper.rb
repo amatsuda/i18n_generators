dfd0d19f6a890d0430333fe3a88fd80c031e3a81
Akira Matsuda <ronnie@dio.jp>
2012-03-13 16:05:38 +0900

    let tests run with Ruby 1.9 + Rails 3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,13 @@
-require 'active_support/all'
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'rails'
+require 'rails/generators'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+Rails.logger ||= Logger.new STDOUT
 
 RSpec.configure do |config|
   # config.mock_with :rr

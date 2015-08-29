3ff622c4be4bb316faf78def7312e6c908a86657
Akira Matsuda <ronnie@dio.jp>
2015-08-30 05:02:09 +0900

    Simplify GoogleTranslate2

--- lib/generators/i18n_translation/lib/translator.rb
+++ lib/generators/i18n_translation/lib/translator.rb
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-require 'open-uri'
 
 module I27r
   class TranslationError < StandardError; end
@@ -25,26 +24,8 @@ module I27r
   module GoogleTranslate2
     def _translate(word, lang)
       w = CGI.escape ActiveSupport::Inflector.humanize(word)
-      json = OpenURI.open_uri("http://ajax.googleapis.com/ajax/services/language/translate?v=1.0&q=#{w}&langpair=en%7C#{lang}").read
-      json = OpenURI.open_uri("https://translate.google.com/translate_a/single?client=t&sl=en&tl=#{lang}&hl=zh-CN&dt=bd&dt=ex&dt=ld&dt=md&dt=qc&dt=rw&dt=rm&dt=ss&dt=t&dt=at&dt=sw&ie=UTF-8&oe=UTF-8&otf=1&ssel=0&tsel=0&q=#{w}",
-                              "accept-language" => "en-US,en;q=0.8",
-                              "user-agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36",
-                              "accept" => "*/*'",
-                              "referer" => "https://translate.google.com/"
-                              ).read
-      index = json.index("],[")
-      json = a = json[2..index]
-      result = if RUBY_VERSION >= '1.9'
-        require 'json'
-        ::JSON.parse json
-      else
-        ActiveSupport::JSON.decode(json)
-      end
-      if result.length == 2
-        result[0]
-      else
-        raise TranslationError.new result.inspect
-      end
+      text = Net::HTTP.get URI("https://translate.google.com/translate_a/single?client=t&sl=en&tl=#{lang}&dt=t&q=#{w}")
+      text.scan(/"(.*?)"/).first.first.tap {|t| t.force_encoding(Encoding::UTF_8) if t.respond_to? :force_encoding}
     end
   end
 
@@ -63,6 +44,7 @@ module I27r
   module BingTranslator
     def _translate(word, lang)
       require 'cgi'
+      require 'open-uri'
 
       w = CGI.escape ActiveSupport::Inflector.humanize(word)
       json = OpenURI.open_uri("http://api.microsofttranslator.com/v2/ajax.svc/TranslateArray?appId=%22T5y_QKkSEGi7P462fd0EwjEhB0_XGUl8PNTgQylxBYks*%22&texts=[%22#{w}%22]&from=%22en%22&to=%22#{lang}%22").read.gsub(/\A([^\[]+)/, '')
